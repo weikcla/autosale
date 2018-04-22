46d72c1e9fdbb1f37d42d7cf73cd9f43b4b8c2e1
Jose Jimenez <jimenez.josear@gmail.com>
2018-04-22 18:48:54 -0300

    Add Vuex to project

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import {store} from './store'
 import router from './router'
 import {
   Vuetify,
@@ -37,6 +38,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
